ce2a0a1d3ecb7fc5e0fb997243ceb81abbd0d1c9
Juan1Fabian <1539806@senati.pe>
2025-04-28 17:36:03 -0500

    Entregable v1.1

--- config/db.sql
+++ config/db.sql
@@ -8,9 +8,14 @@ CREATE TABLE productos
     producto	VARCHAR(20) NOT NULL,
     imagen		BLOB,
     precio		DECIMAL NOT NULL,
-	marca 		VARCHAR(20) NOT NULL,
     modelo		VARCHAR(20) NOT NULL,
     descripcion	VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE marcas
+(
+	idmarca 	INT AUTO_INCREMENT PRIMARY KEY,
+    marca		VARCHAR(20) UNIQUE NOT NULL
 );
 
 CREATE TABLE catalogos
@@ -18,4 +23,29 @@ CREATE TABLE catalogos
 	idcatalogo 	INT AUTO_INCREMENT PRIMARY KEY,
     catalogo	VARCHAR(20) NOT NULL
     
-)
+);
+
+INSERT INTO marcas(marca) VALUES
+('LG'),
+('Samsumg'),
+('Philip'),
+('Telefunken'),
+('Xiaomi'),
+('Panasonic'),
+('Sony'),
+('TCL');
+
+INSERT INTO catalogos(catalogo) VALUES
+('Smartv'),
+('Microondas'),
+('Celulares'),
+('Monitores'),
+('Proyectores'),
+('Tablet');
+
+INSERT INTO productos(idproducto, producto, imagen, precio, modelo, descripcion) VALUES
+('1', 'SmartTV', NULL, '100', 'A-67', 'Un buen televisor de 50p');
+
+SELECT * FROM catalogos
+
+
